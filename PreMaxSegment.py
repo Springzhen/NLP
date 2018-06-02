b4b6c9379f154d813babb26d1fe917b9f5fe2997
Huachun Zhen <1120978081@qq.com>
2018-06-02 16:38:09 +0800

    Update PreMaxSegment.py

--- PreMaxSegment.py
+++ PreMaxSegment.py
@@ -7,10 +7,7 @@ Created on Sat Jun  2 15:22:40 2018
 
 """
 
-
-
-
-def PreMaxSeg(sen,dicts,span):
+def PreMaxSeg1(sen,dicts,span):
     '''前向最大匹配算法'''
     idx = 0
     while idx<len(sen):
@@ -27,13 +24,28 @@ def PreMaxSeg(sen,dicts,span):
             yield sen[idx]
         idx += i
 
-
+def PreMaxSeg2(sen,dicts,span):
+    cur,tail = 0,span
+    while (cur < tail and cur<len(sen)):
+        if len(sen)<span:
+            tail = len(sen)
+        
+        if tail == cur+1:
+            yield sen[cur:tail]
+            cur +=1
+            tail = cur+span
+        elif sen[cur:tail] in dicts:
+            yield sen[cur:tail]
+            cur=tail
+            tail = cur+span
+        else:
+            tail-=1
 
 if __name__=="__main__":
     dicts = [u'有些', u'大学生', u'眼高手低', u'小事情']
     sen = u'有些大学生眼高手低，不屑于做小事情。'
     span = 4
-    print('/'.join(PreMaxSeg(sen,dicts,span)))
+    print('/'.join(PreMaxSeg1(sen,dicts,span)))
 
 
 
